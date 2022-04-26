dc3cbca949d65a6340a674b7430ff80dc4f3cfc9
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-04-26 20:41:46 -0300

    Update with layout component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,10 @@
-import { Typography } from '@mui/material';
 import type { NextPage } from 'next';
-import Head from 'next/head';
+import Layout from '../components/Layout';
 
 const Home: NextPage = () => {
   return (
     <div>
-      <Head>
-        <title>Sanity Amazon</title>
-        <meta
-          name='description'
-          content='A ecommerce website created with nextjs and sanity'
-        />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Typography component='h1' variant='h1'>
-        Sanity Amazon
-      </Typography>
+      <Layout>List Products</Layout>
     </div>
   );
 };
