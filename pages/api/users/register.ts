c40b36b057edb757a530fd3542926b36bff71b7b
Victor Azevedo <vitinsilva0xd@gmail.com>
2022-04-28 16:18:05 -0300

    Add check for duplicated email

--- pages/api/users/register.ts
+++ pages/api/users/register.ts
@@ -1,8 +1,9 @@
-import { NextApiRequest, NextApiResponse } from 'next';
 import nc from 'next-connect';
 import bcrypt from 'bcryptjs';
 import axios from 'axios';
 import config from '../../../utils/config';
+import client from '../../../utils/client';
+import { NextApiRequest, NextApiResponse } from 'next';
 import { signToken } from '../../../utils/auth';
 
 const handler = nc<NextApiRequest, NextApiResponse>();
@@ -24,6 +25,17 @@ handler.post(async (req, res) => {
     },
   ];
 
+  const existUser = await client.fetch(
+    '*[_type == "user" && email == $email][0]',
+    {
+      email: req.body.email,
+    }
+  );
+
+  if (existUser) {
+    return res.status(401).send({ message: 'Email already exists' });
+  }
+
   const { data } = await axios.post(
     `https://${projectId}.api.sanity.io/v1/data/mutate/${dataset}?returnIds=true`,
     {
@@ -36,8 +48,6 @@ handler.post(async (req, res) => {
       },
     }
   );
-
-  console.log(data);
 
   const userId = data.results[0].id;
   const user = {
